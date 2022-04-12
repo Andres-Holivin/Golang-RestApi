29598df12d6a175d2f8e1c898fa5da77c28a629a
Andres Holivin <andres.holivin@binus.edu>
2022-04-12 22:02:11 +0700

    init

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -55,3 +55,61 @@ func GetAUser(c *fiber.Ctx) error {
 	}
 	return c.Status(http.StatusOK).JSON(responses.UserResponses{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": user}})
 }
+func EditAUser(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	userId := c.Params("userId")
+	var user models.User
+	defer cancel()
+	objId, _ := primitive.ObjectIDFromHex(userId)
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.UserResponses{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+	if validationErr := validate.Struct(&user); validationErr != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.UserResponses{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": validationErr.Error()}})
+	}
+	update := bson.M{"name": user.Name, "location": user.Location, "title": user.Title}
+	result, err := userCollection.UpdateOne(ctx, bson.M{"_id": objId}, bson.M{"$set": update})
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.UserResponses{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+	var updateUser models.User
+	if result.MatchedCount == 1 {
+		err := userCollection.FindOne(ctx, bson.M{"_id": objId}).Decode(&updateUser)
+		if err != nil {
+			return c.Status(http.StatusBadRequest).JSON(responses.UserResponses{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+		}
+	}
+	return c.Status(http.StatusOK).JSON(responses.UserResponses{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": updateUser}})
+}
+func DeleteAUser(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	userId := c.Params("userId")
+	defer cancel()
+	objId, _ := primitive.ObjectIDFromHex(userId)
+	result, err := userCollection.DeleteOne(ctx, bson.M{"_id": objId})
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.UserResponses{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+	if result.DeletedCount < 1 {
+		return c.Status(http.StatusBadRequest).JSON(responses.UserResponses{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": "user not found"}})
+	}
+	return c.Status(http.StatusOK).JSON(responses.UserResponses{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": "user deleted"}})
+}
+func GetAllUsers(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var user []models.User
+	defer cancel()
+	result, err := userCollection.Find(ctx, bson.M{})
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.UserResponses{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+	defer result.Close(ctx)
+	for result.Next(ctx) {
+		var singleUser models.User
+		if err = result.Decode(&singleUser); err != nil {
+			return c.Status(http.StatusBadRequest).JSON(responses.UserResponses{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+		}
+		user = append(user, singleUser)
+	}
+	return c.Status(http.StatusOK).JSON(responses.UserResponses{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": user}})
+}
--- routes/user_route.go
+++ routes/user_route.go
@@ -9,4 +9,7 @@ import (
 func UserRoute(app *fiber.App) {
 	app.Post("/users", controllers.CreateUser)
 	app.Get("/user/:userId", controllers.GetAUser)
+	app.Put("user/:userId", controllers.EditAUser)
+	app.Delete("/user/:userId", controllers.DeleteAUser)
+	app.Get("/users", controllers.GetAllUsers)
 }
